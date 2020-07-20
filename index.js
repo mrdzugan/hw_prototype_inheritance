ce29bd4ab21cecd6ae39275145543924f81aacc7
Andrei Dzugan <mr.dzugan@gmail.com>
2020-07-21 00:58:24 +0300

    Add map() method

--- index.js
+++ index.js
@@ -71,3 +71,11 @@ myArrayProto.filter = function (callback) {
 	}
 	return arr;
 }
+
+myArrayProto.map = function (callback) {
+	const arr = new MyArray();
+	for (let i = 0; i < this.length; i++) {
+		arr.push(callback(this[i], i, this));
+	}
+	return arr;
+}
