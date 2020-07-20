46e4e6164a15d9d4e07098577e8d548f988ab95a
Andrei Dzugan <mr.dzugan@gmail.com>
2020-07-21 00:09:38 +0300

    Add find method & refactor code

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-function MyArray() {
+'use strict';
+
+function MyArray(...args) {
+	if (!new.target) {
+		return new MyArray(...args);
+	}
 	this.length = 0;
+	if (args.length) {
+		for (const item of args) {
+			this[this.length++] = item;
+		}
+	}
 }
 
-const MyArrayProto = new MyArray();
+const myArrayProto = new MyArray();
 
-MyArray.prototype = MyArrayProto;
+MyArray.prototype = myArrayProto;
 
-MyArrayProto.push = function (...args) {
+myArrayProto.push = function (...args) {
 	for (let i = 0; i < args.length; i++) {
 		this[++this.length] = args[i];
 	}
 	return this.length;
 }
+
+myArrayProto.find = function (callback) {
+	for (let i = 0; i < this.length; i++) {
+		if (callback(this[i], i, this)) {
+			return this[i];
+		}
+	}
+}
+
